6186e95f87c3bdfc5752cef096cd815c6b8c4784
ishanvepa <ishanvepa171@gmail.com>
2024-04-10 16:10:59 -0400

    added rounded corners to project cards

--- components/project.tsx
+++ components/project.tsx
@@ -28,7 +28,7 @@ imageUrl
             opacity: scrollYProgress,
         }}
 
-    className="group bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem]mb-3 sm:mb-8 last:mb-0 even:pl-8 hover:bg-gray-200 transition">
+    className="group bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem]mb-3 sm:mb-8 last:mb-0 even:pl-8 hover:bg-gray-200 transition rounded-xl">
         <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full group-even:ml-[18rem]">
             <h3 className="text-2xl font-semibold">{title}</h3>
             <p className="mt-2 leading-relaxed text-gray-700">{description}</p>
