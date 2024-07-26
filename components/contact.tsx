8037f1a1ef668262fbe397965f51af1681ec053a
ishanvepa <ishanvepa171@gmail.com>
2024-07-25 22:24:39 -0400

    contact me header adjustment

--- components/contact.tsx
+++ components/contact.tsx
@@ -6,7 +6,7 @@ import { motion } from 'framer-motion'
 
 export default function ContactMe() {
   return (
-    <motion.section className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-50"
+    <motion.section className="mt-[8rem] mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-50"
       initial={{opacity: 0, y: 100}}
       animate={{opacity: 1, y: 0}}
       transition={{delay: 0.175}}
