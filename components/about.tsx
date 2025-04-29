e3c3d3356abeaf3e2cc0628de3ad18a5f70d7eea
ishanvepa <ishanvepa171@gmail.com>
2025-04-29 03:37:32 -0400

    edited about me

--- components/about.tsx
+++ components/about.tsx
@@ -16,7 +16,7 @@ export default function About() {
         
         <p className="mb-3">I have an avid interest in technology and computer science. I am currently pursuing a <span className="font-bold">Bachelor of Science in Computer Science</span> at the <a className="font-bold hover:opacity-65 hover:text-green-400 transition-opacity ease-in-out" href="https://gatech.edu">Georgia Institute of Technology</a> with a concentration in Human-Computer Interaction.
         </p>
-        <p> I also pursue creative passions such as video production and music. I have produced documentaries, and am a jazz saxophonist musician.</p>
+        <p> When I am not programming you may find me transcribing jazz records, trying new cold brews, or watching premier league soccer (GGMU ifykyk).</p>
     </motion.section>
   )
 }
